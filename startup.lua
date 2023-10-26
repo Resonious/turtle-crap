bc81c4bcbb6fc4b98415230ca6062c9252b2a0f8
Nigel Baillie <nigelbaillie@hey.com>
2023-10-26 18:10:44 +0900

    more

--- startup.lua
+++ startup.lua
@@ -8,6 +8,10 @@ local file, err = fs.open("state", "r")
 
 if err then
   say("Hello")
+elseif fs.exists("go") then
+  say("Turtle starting dig!")
+  fs.delete("go")
+  shell.execute("ex.lua", "6")
 else
   say("Hi. I'm at " .. file.readLine())
   file.close()
