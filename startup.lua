ff20ba35fbab670d61e750e6a56b431968712777
Nigel Baillie <nigelbaillie@hey.com>
2023-10-26 17:11:57 +0900

    initial

---
+++ startup.lua
@@ -0,0 +1,13 @@
+function say(msg)
+    print(msg)
+    peripheral.call("left", "sendMessage", msg)
+    http.post("https://hook.snd.one/nigel/mcturtle", msg)
+end
+
+local file, err = fs.open("state", "r")
+
+if err then
+  say("Hello")
+else
+  say("Hi. I'm at " .. file.readLine())
+end
